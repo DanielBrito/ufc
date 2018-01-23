e2f62f38a8833fee9599e71656bfb007c1bf7aac
Daniel Brito <daniell-henrique@hotmail.com>
2018-01-23 13:49:29 -0200

    Update fibonacci.c

--- Matemática Discreta/Algoritmos/fibonacci.c
+++ Matemática Discreta/Algoritmos/fibonacci.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 
-/*  Baseado no exercício:
+/*  Baseado no exercÃ­cio:
 	
-	Mostre que o e-nésimo número da sequência de Fibonacci
-	é dado pela fórmula abaixo:
+	Mostre que o n-Ã©simo nÃºmero da sequÃªncia de Fibonacci
+	Ã© dado pela fÃ³rmula abaixo:
 	
 	Fn = (pow(((1+sqrt(5))/2), 2) - pow(((1-sqrt(5))/2), n) / sqrt(5)	
 */
