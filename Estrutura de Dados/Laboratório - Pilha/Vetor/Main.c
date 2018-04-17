612a3f0c01444a9473fd6e46d156ce138fa26f41
Daniel Brito <daniell-henrique@hotmail.com>
2018-04-17 19:03:48 -0300

    Add files via upload

---
+++ Estrutura de Dados/Laboratório - Pilha/Vetor/Main.c
@@ -0,0 +1,189 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include"Pilha.h"
+
+/* 1 - Função para comparar duas pilhas. Se forem iguais, retorna 1, caso contrário, retorna 0: */
+int compara(Pilha* p1, Pilha* p2);
+
+/* 2 - Função para criar uma cópia de uma pilha passada por parâmentro: */
+Pilha* copia(Pilha* p);
+
+/* Função auxiliar para inverter uma pilha: */
+Pilha* inverte(Pilha* p);
+
+int main(){
+
+    Pilha* p1;
+    Pilha* p2;
+    Pilha* p3;
+    Pilha* pC;
+   	int i;
+   	float x;
+
+    p1 = p_cria();
+    p2 = p_cria();
+    p3 = p_cria();
+    pC = p_cria();
+
+    x=0;
+
+    for(i=0; i<5; i++){
+        
+        p_push(p1, x);
+        ++x;
+    }
+
+    x=0;
+
+    for(i=0; i<5; i++){
+        
+        p_push(p2, i);
+        ++x;
+    }
+
+    printf("Teste 1: ");
+
+    if(compara(p1, p2)==1){
+
+    	printf("Pilhas iguais.\n");
+    }
+    else{
+
+    	printf("Pilhas diferentes.\n");
+    }
+
+    p1 = p_cria();
+    p2 = p_cria();
+
+    x=1;
+
+    for(i=0; i<5; i++){
+        
+        p_push(p1, x);
+        ++x;
+    }
+
+    x=1;
+
+    for(i=0; i<9; i++){
+        
+        p_push(p2, x);
+        ++x;
+    }
+
+    printf("\nTeste 2: ");
+
+    if(compara(p1, p2)==1){
+
+    	printf("Pilhas iguais.\n");
+    }
+    else{
+
+    	printf("Pilhas diferentes.\n");
+    }
+
+    for(i=9; i>=0; i--){
+        
+        p_push(p3, i);
+    }
+
+    pC = copia(p3);
+
+    printf("\nCópia da Pilha 3:\n\n");
+
+    while(!p_vazia(pC)){
+
+    	printf("%.2f\n", p_pop(pC));
+    }
+
+    p_libera(p1);
+    p_libera(p2);
+    p_libera(p3);
+    p_libera(pC);
+
+    return 0;
+}
+
+int compara(Pilha* p1, Pilha* p2){
+
+	float aux1, aux2;
+	int diferentes = 0;
+
+	Pilha* auxP1 = p_cria();
+	Pilha* auxP2 = p_cria();
+
+	while(!p_vazia(p1) && !p_vazia(p2)){
+
+		aux1 = p_pop(p1); 
+		aux2 = p_pop(p2);
+
+		if(aux1!=aux2){
+
+			diferentes = 1;
+		}
+
+		p_push(auxP1, aux1);
+		p_push(auxP2, aux2);
+    }
+
+    auxP1 = inverte(auxP1);
+	auxP2 = inverte(auxP2);
+
+	while(!p_vazia(auxP1) && !p_vazia(auxP2)){
+
+		aux1 = p_pop(auxP1);
+		aux2 = p_pop(auxP2);
+
+		p_push(p1, aux1);
+		p_push(p2, aux2);
+	}
+
+    if(diferentes==1){
+
+    	return 0;
+    }
+    else{
+
+    	return 1;
+    }
+}
+
+Pilha* inverte(Pilha* p){
+
+	Pilha* c = p_cria();
+	Pilha* aux = p_cria();
+
+	float x;
+
+	while(!p_vazia(p)){
+
+		x = p_pop(p);
+		p_push(c, x);
+		p_push(aux, x);
+	}
+
+	while(!p_vazia(aux)){
+
+		x = p_pop(aux);
+		p_push(p, x);
+	}
+
+	p_libera(aux);
+
+	return c;
+}
+
+Pilha* copia(Pilha* p){
+
+	Pilha* c = p_cria();
+
+	while(!p_vazia(p)){
+
+		p_push(c, p_pop(p));
+	}
+
+	c = inverte(c);
+
+	return c;
+}
