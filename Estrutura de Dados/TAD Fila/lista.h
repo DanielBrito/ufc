f1ae74e8e273b1f601bdddcd0b3ce034978260a1
Daniel Brito <daniell-henrique@hotmail.com>
2018-05-05 13:25:09 -0300

    Delete lista.h

--- Estrutura de Dados/TAD Fila/lista.h
+++
@@ -1,9 +0,0 @@
-typedef struct lista Lista;
-
-Lista* listaCria();
-Lista* listaInsere(Lista* l, float v);
-Lista* listaRemoveInicio(Lista* l);
-float listaRetornaInicio(Lista* l);
-int listaVazia(Lista* l);
-void setProx(Lista* l, Lista* p);
-void listaLibera(Lista* l);
