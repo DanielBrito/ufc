11fac4301a2a226b6d9b32eb1b5e72a5ea0d3b30
Daniel Brito <daniell-henrique@hotmail.com>
2018-05-05 13:24:53 -0300

    Delete fila.h

--- Estrutura de Dados/TAD Fila/fila.h
+++
@@ -1,8 +0,0 @@
-typedef struct fila Fila;
-
-Fila* filaCria();
-void filaInsere(Fila* f, float v);
-float filaRetira(Fila* f);
-int filaVazia(Fila* f);
-void filaLibera(Fila* f);
-void filaImprime(Fila* f);
