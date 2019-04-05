d694decd6b4e41af165efb2156a11014efacd1ff
Daniel Brito <daniell-henrique@hotmail.com>
2019-04-04 21:03:11 -0300

    Add files via upload

--- Monitoria - Fundamentos de Programação/2019.1/Tabela Verdade (for).c
+++ Monitoria - Fundamentos de Programação/2019.1/Tabela Verdade (for).c
@@ -1,10 +1,14 @@
+/* Análogo ao Tabela Verdade.c, porém, utilizando laço de repetição */
+
 #include<stdio.h>
 
 int main(){
 	
-	int P, Q, R;
+	int P, Q, R; // Como existem 3 átomos, temos 2^3 possíveis combinações de valores possíveis, ou seja, 8 linhas
 	
-	printf("P  Q  R  (P && R)  (Q || R)  (P && R) || (Q || R)\n\n");
+	printf("P  Q  R  (P && R)  (Q || R)  (P && R) || (Q || R)\n\n"); // Cabeçalho da tabela com as proposições lógicas
+
+    // Laço de repetição para verificar o valor lógico de cada uma das combinações possíveis:
 	
 	for(P=0; P<=1; P++){
 		
