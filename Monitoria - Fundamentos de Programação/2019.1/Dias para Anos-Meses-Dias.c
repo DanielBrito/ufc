c93f3dec7d6dc6aad0110dcdb6d5f447e7737a4b
Daniel Brito <daniell-henrique@hotmail.com>
2019-04-21 14:20:11 -0300

    Add files via upload

--- Monitoria - Fundamentos de Programação/2019.1/Dias para Anos-Meses-Dias.c
+++ Monitoria - Fundamentos de Programação/2019.1/Dias para Anos-Meses-Dias.c
@@ -7,10 +7,10 @@ int main(){
 	printf("Digite sua idade em dias: ");
 	scanf("%d", &dias);
 	
-	anos = dias/365;
-	resto = dias%365;
-	meses = resto/30;
-	dias = resto%30;
+	anos = dias/365; // Divisao inteira para sabermos quantos anos (de 365 dias) podem ser formados com x dias
+	resto = dias%365; // O que sobrar da divisão anterior, utilizaremos para calcular os meses
+	meses = resto/30; // Divisao inteira para sabermos quantos meses (de 30 dias) cabe no resto da divisão anterior
+	dias = resto%30; // O que sobrar da divisão anterior, sabemos que são os dias remanescentes
 	
 	printf("\nSua idade e dada por:\n\n");
 	printf("%d ano(s)\n", anos);
